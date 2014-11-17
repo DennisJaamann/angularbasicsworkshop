b670cc24d547f6aa40926a5deb0cb688b51d9fea
Dennis Jaamann <dennis.jaamann@ordina.be>
2014-11-17 21:26:33 +0100

    Removed all TODOS

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,9 +10,7 @@
    * Main module of the application.
    */
   angular
-    .module('solutionApp', []) //TODO: 3. create a controller 'ApplicationController' on the module, pass $scope and $log
-    .controller('ApplicationController', function ($scope, $log) {
-      $log.log('Initializing application'); //TODO: 4. log 'initializing application'
-      $scope.hello = 'Hello Ordina'; //TODO: 5. create a scope variable called 'hello'
-    });
+    .module('solutionApp', []); //TODO: 3. create a controller 'ApplicationController' on the module, pass $scope and $log
+      //TODO: 4. log 'initializing application'
+      //TODO: 5. create a scope variable called 'hello'
 })();
